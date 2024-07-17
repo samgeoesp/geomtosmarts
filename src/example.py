688ae5f0d8c1b6c7583869c90a0695ac83936734
Sam Espley <sge28@bath.ac.uk>
2024-07-17 21:05:11 +0100

    Add changes to example - looping through multiple files.

--- src/example.py
+++ src/example.py
@@ -1,11 +1,30 @@
+"""
+Example code for how to use geomtosmarts.py within a python script.
+"""
+
+# Import packages.
+import os
 from geomtosmarts import geo
 
-smiles, smarts = geo('../examples/diels_alder.out', r=True, k=True, n=True)
 
+# Only loading in one structure.
+smiles, smarts = geo('../examples/diels_alder.out', r=False, k=False, n=False)
 print(f'Reaction SMARTS:\n{smarts}')
 print('---')
 print(f'Reaction SMILES:\n{smiles}')
 
+
+# Loading multiple files.
+data_dict = {}
+for file in os.listdir('../examples/'):
+    path = f'../examples/{file}'
+    smiles, smarts = geo(path, r=False, k=False, n=False)
+    data_dict[file] = {'rxn_smiles':smiles, 'rxn_smarts':smarts}
+    
+print('---')
+print(f'Files passed = {len(data_dict)}')
+print('---')
+print(f'Data:\n{data_dict}')
 
 """
 Command Line Usage:
